d12911e5b8b398f6aa1ce1d5a2c2938bd74d9d0c
Adrian <bluevue@protonmail.com>
2019-03-07 20:24:34 -0500

    Changed to accomidate testing functions.

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,31 @@ import java.util.Scanner;
 
 public class Main {
 
-	//test update
-	
+		
 	public static void main(String[] args) throws AWTException {
+		
+		
 		for (int i = 0; i < 5; i++) {
 			Menu.cmbTab();
 			Menu.skillsTab();
+			Menu.questTab();
+			Menu.invTab();
+			Menu.equipTab();
+			Menu.prayTab();
+			Menu.mageTab();
+			
+			Menu.clanTab();
+			Menu.friendTab();
+			Menu.accTab();
+			Menu.logTab();
+			Menu.optionTab();
+			Menu.emotesTab();
+			Menu.musicTab();
+			
+			Actions.setCamera();
+			Actions.bankLumby();
+			Actions.bankAll();
+			Actions.exitBank();
 		}
 		
 	}
