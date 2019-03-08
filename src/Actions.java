5c5603424c640912449356734406d424f629ed47
Adrian <bluevue@protonmail.com>
2019-03-07 20:23:50 -0500

    Added deposit all & exit bank.

--- src/Actions.java
+++ src/Actions.java
@@ -13,10 +13,12 @@ import java.awt.event.KeyEvent;
 import java.util.Random;
 
 public class Actions {
+	
+	// sets camera to a reliable location
 	public static void setCamera() throws AWTException {
-		// Creates bot
+		// creates bot
 		Robot bot = new Robot();
-
+		
 		// moves mouse to compass to set north and center view.
 		bot.mouseMove(824, 72);
 		bot.delay(0);
@@ -26,11 +28,7 @@ public class Actions {
 		bot.delay(160);
 
 		// Clicks Options
-		bot.mouseMove(941, 528);
-		bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-		bot.delay(101);
-		bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-		bot.delay(98);
+		Menu.optionTab();
 
 		// Zooms all the way in
 		bot.mouseMove(967, 320);
@@ -40,6 +38,54 @@ public class Actions {
 		bot.delay(1000);
 	}
 
+	// banks all items in invy
+	public static void bankAll() throws AWTException {
+		// finds X axis
+		Random randomX = new Random();
+		int lowX = 693;
+		int randX = randomX.nextInt(24);
+		randX += lowX;
+
+		// finds Y axis
+		Random randomY = new Random();
+		int lowY = 350;
+		int randY = randomY.nextInt(24);
+		randY += lowY;
+
+		// creates robot, moves cursor and clicks
+		Robot bot = new Robot();
+		bot.delay(1000);
+		bot.mouseMove(randX, randY);
+		bot.delay(RandNumGen.pressReleaseRand());
+		bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+		bot.delay(RandNumGen.pressReleaseRand());
+		bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+	}
+
+	public static void exitBank() throws AWTException {
+		// finds X axis
+		Random randomX = new Random();
+		int lowX = 740;
+		int randX = randomX.nextInt(14);
+		randX += lowX;
+
+		// finds Y axis
+		Random randomY = new Random();
+		int lowY = 67;
+		int randY = randomY.nextInt(14);
+		randY += lowY;
+
+		// creates robot, moves cursor and clicks
+		Robot bot = new Robot();
+		bot.delay(1000);
+		bot.mouseMove(randX, randY);
+		bot.delay(RandNumGen.pressReleaseRand());
+		bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+		bot.delay(RandNumGen.pressReleaseRand());
+		bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+	}
+
+	// opens bank in lumby
 	public static void bankLumby() throws AWTException {
 		Robot bot = new Robot();
 		bot.mouseMove(540, 150);
